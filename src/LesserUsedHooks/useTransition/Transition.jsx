02650a1a8e7988132d61a8ff4769b743cbd0bccc
Ciprian <cozma_ciprian19@yahoo.com>
2023-03-05 16:11:49 +0200

    added useTransition

--- src/LesserUsedHooks/useTransition/Transition.jsx
+++ src/LesserUsedHooks/useTransition/Transition.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState, useTransition } from "react";
 
 export default function Transition() {
-  return <div>Transition</div>;
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+  const [isPending, startTransition] = useTransition();
+
+  const LIST_SIZE = 20000;
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    startTransition(() => {
+      const l = [];
+      for (let i = 0; i <= LIST_SIZE; i++) {
+        l.push(e.target.value);
+      }
+      setList(l);
+    });
+  };
+
+  return (
+    <div>
+      <input type={"text"} value={input} onChange={handleChange} />
+      {isPending
+        ? "Loading..."
+        : list.map((item, idx) => {
+            return <div key={idx}> {item} </div>;
+          })}
+    </div>
+  );
 }
