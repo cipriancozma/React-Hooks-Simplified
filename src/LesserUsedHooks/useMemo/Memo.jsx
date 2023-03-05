dc61c474b2a53492b895b1c82cbfdaa3864e5162
Ciprian <cozma_ciprian19@yahoo.com>
2023-03-05 12:03:20 +0200

    added useMemo

--- src/LesserUsedHooks/useMemo/Memo.jsx
+++ src/LesserUsedHooks/useMemo/Memo.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useMemo, useEffect } from "react";
 
 function Memo() {
-  return <div>Memo</div>;
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+
+  const themeStyles = useMemo(() => {
+    return {
+      backgroundColor: dark ? "black" : "white",
+      color: dark ? "white" : "black",
+    };
+  }, [dark]);
+
+  useEffect(() => {
+    console.log("Theme changed");
+  }, [themeStyles]);
+
+  return (
+    <>
+      <input
+        type={"number"}
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change Theme
+      </button>
+      <div style={themeStyles}>{doubleNumber}</div>
+    </>
+  );
+}
+
+function slowFunction(num) {
+  console.log("Calling Slow Function");
+  for (let i = 0; i <= 1000000000; i++) {}
+  return num * 2;
 }
 
 export default Memo;
